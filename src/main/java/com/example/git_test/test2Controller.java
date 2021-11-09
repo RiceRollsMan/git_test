32eefe26eca0681cb81d58ecd85ff221fdc3f06f
tao Fu <72638380+RiceRollsMan@users.noreply.github.com>
2021-11-10 00:33:49 +0800

    Update test2Controller.java

--- src/main/java/com/example/git_test/test2Controller.java
+++ src/main/java/com/example/git_test/test2Controller.java
@@ -2,7 +2,7 @@ package com.example.git_test;
 
 public class test2Controller {
     public String abc(){
-        System.out.println("abc");
+        System.out.println("abc打");
         System.out.println("abc");
         System.out.println("abc");
         System.out.println("嘿嘿");

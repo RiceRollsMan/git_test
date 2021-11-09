dacd221495267196228b8c20f475b477074f2ff9
RiceRollsMan <3533734346@qq.com>
2021-11-09 23:51:05 +0800

    firstCommit

---
+++ src/main/java/com/example/git_test/test2Controller.java
@@ -0,0 +1,7 @@
+package com.example.git_test;
+
+public class test2Controller {
+    public String abc(){
+        return "c";
+    }
+}
---
+++ src/main/java/com/example/git_test/testController.java
@@ -0,0 +1,10 @@
+package com.example.git_test;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class testController {
+    public String abc(){
+        return "abc";
+    }
+}

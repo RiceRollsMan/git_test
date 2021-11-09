61c1716c065db1c3f209f30c42ee5c5dbd1b5b70
RiceRollsMan <3533734346@qq.com>
2021-11-10 00:31:17 +0800

    devCommit1

--- src/main/java/com/example/git_test/test2Controller.java
+++ src/main/java/com/example/git_test/test2Controller.java
@@ -3,6 +3,8 @@ package com.example.git_test;
 public class test2Controller {
     public String abc(){
         System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
         System.out.println("嘿嘿");
         System.out.println("嘿嘿");
         System.out.println("嘿嘿");
